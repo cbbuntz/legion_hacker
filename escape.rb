81e6d3b8c97ab8f63397e507f1242d4b26ad4c8b
cbbuntz <Jason Buntz>
2018-02-01 16:20:51 -0600

    formatting

--- escape.rb
+++ escape.rb
@@ -2,13 +2,13 @@ require './util.rb'
 
 module EscSequence
     @ansi_color = {
-        black:	 [30, 40],
-        red:	 [31, 41],
-        green:	 [32, 42],
-        yellow:	 [33, 43],
-        blue:	 [34, 44],
+        black:   [30, 40],
+        red:     [31, 41],
+        green:   [32, 42],
+        yellow:  [33, 43],
+        blue:    [34, 44],
         magenta: [35, 45],
-        cyan:	 [36, 46],
+        cyan:    [36, 46],
         white:   [37, 47],
     }
 
