99d74c65bada1856aefcb8b87780e93a4793ec54
cbbuntz <Jason Buntz>
2018-02-02 08:42:22 -0600

    add figlet fonts

--- c/color.h
+++ c/color.h
@@ -25,21 +25,3 @@ char truecolor_fmt[] = "\e[%c8;2;%u;%u;%um";
 int truecolor_print(Truecolor c);
 int truecolor_str(char *s, Truecolor c);
 
-Truecolor fhsv2rgb(float, float, float, uint32_t);
-Truecolor fhsl2rgb(float, float, float, uint32_t);
-
-Truecolor hsv2rgb(HSV, uint32_t);
-Truecolor hsl2rgb(HSL, uint32_t);
-
-Truecolor fhsv2rgb_fg(float, float, float);
-Truecolor fhsv2rgb_bg(float, float, float);
-
-Truecolor fhsl2rgb_fg(float, float, float);
-Truecolor fhsl2rgb_bg(float, float, float);
-
-Truecolor hsv2rgb_fg(HSV);
-Truecolor hsv2rgb_bg(HSV);
-
-Truecolor hsl2rgb_fg(HSL);
-Truecolor hsl2rgb_bg(HSL);
-
